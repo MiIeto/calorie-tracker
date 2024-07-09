ecb33f470773d1afc21182974b5c4630eaab11d2
Mileto <milenkotomasevic41@gmail.com>
2024-07-09 01:45:06 -0400

    dev: Added Calorie Tracker

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { useReducer, useEffect, useMemo } from "react";
 import Form from "./components/Form";
 import { activityReducer, initialState } from "./reducers/activity-reducer";
 import ActivityList from "./components/ActivityList";
+import CalorieTracker from "./components/CalorieTracker";
 
 function App() {
 	const [state, dispatch] = useReducer(activityReducer, initialState);
@@ -12,13 +13,13 @@ function App() {
 
 	// eslint-disable-next-line react-hooks/rules-of-hooks
 	const canRestartApp = () =>
-		// eslint-disable-next-line react-hooks/rules-of-hooks
+		// eslint-disable-next-line react-hooks/rules-of-hooks, react-hooks/exhaustive-deps
 		useMemo(() => state.activities.length, [state.activities]);
 
 	return (
 		<>
 			<header className="bg-lime-600 py-3">
-				<div className="max-w-4xl mx-auto flex justify-between">
+				<div className="max-w-4xl mx-auto flex justify-between items-center">
 					<h1 className="text-center text-lg font-bold text-white uppercase">
 						Contador de Calorias
 					</h1>
@@ -34,6 +35,11 @@ function App() {
 			<section className="bg-lime-500 py-20 px-5">
 				<div className="max-w-4xl mx-auto">
 					<Form dispatch={dispatch} state={state} />
+				</div>
+			</section>
+			<section className="bg-gray-800 py-10">
+				<div className="max-w-4xl mx-auto">
+					<CalorieTracker activities={state.activities} />
 				</div>
 			</section>
 			<section className="p-10 mx-auto max-w-4xl">
