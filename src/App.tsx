08b46d0c37133007350aab91912a76391a715537
Mileto <milenkotomasevic41@gmail.com>
2024-07-31 22:21:59 -0400

    w-context: Updated to usecontext

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,14 @@
-import { useReducer, useEffect, useMemo } from "react";
-import Form from "./components/Form";
-import { activityReducer, initialState } from "./reducers/activity-reducer";
-import ActivityList from "./components/ActivityList";
-import CalorieTracker from "./components/CalorieTracker";
+import { useEffect, useMemo } from 'react';
+import Form from './components/Form';
+import ActivityList from './components/ActivityList';
+import CalorieTracker from './components/CalorieTracker';
+import { useActivity } from './hooks/useActivity';
 
 function App() {
-	const [state, dispatch] = useReducer(activityReducer, initialState);
+	const { state, dispatch } = useActivity();
 
 	useEffect(() => {
-		localStorage.setItem("activities", JSON.stringify(state.activities));
+		localStorage.setItem('activities', JSON.stringify(state.activities));
 	}, [state.activities]);
 
 	const canRestartApp = () =>
@@ -17,35 +17,32 @@ function App() {
 
 	return (
 		<>
-			<header className="bg-lime-600 py-3">
-				<div className="max-w-4xl mx-auto flex justify-between items-center">
-					<h1 className="text-center text-lg font-bold text-white uppercase">
+			<header className='bg-lime-600 py-3'>
+				<div className='max-w-4xl mx-auto flex justify-between items-center'>
+					<h1 className='text-center text-lg font-bold text-white uppercase'>
 						Contador de Calorias
 					</h1>
 					<button
-						className="bg-gray-800 hover:bg-gray-900 p-2 font-bold uppercase text-white cursor-pointer rounded-lg text-sm disabled:opacity-10 disabled:cursor-default"
+						className='bg-gray-800 hover:bg-gray-900 p-2 font-bold uppercase text-white cursor-pointer rounded-lg text-sm disabled:opacity-10 disabled:cursor-default'
 						disabled={!canRestartApp()}
-						onClick={() => dispatch({ type: "restart-app" })}
+						onClick={() => dispatch({ type: 'restart-app' })}
 					>
 						Reiniciar APP
 					</button>
 				</div>
 			</header>
-			<section className="bg-lime-500 py-20 px-5">
-				<div className="max-w-4xl mx-auto">
-					<Form dispatch={dispatch} state={state} />
+			<section className='bg-lime-500 py-20 px-5'>
+				<div className='max-w-4xl mx-auto'>
+					<Form/>
 				</div>
 			</section>
-			<section className="bg-gray-800 py-10">
-				<div className="max-w-4xl mx-auto">
-					<CalorieTracker activities={state.activities} />
+			<section className='bg-gray-800 py-10'>
+				<div className='max-w-4xl mx-auto'>
+					<CalorieTracker />
 				</div>
 			</section>
-			<section className="p-10 mx-auto max-w-4xl">
-				<ActivityList
-					activities={state.activities}
-					dispatch={dispatch}
-				/>
+			<section className='p-10 mx-auto max-w-4xl'>
+				<ActivityList />
 			</section>
 		</>
 	);
